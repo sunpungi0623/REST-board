c7ffec55eaca692428cee77005c8b9d0b327c32f
sunpungi0623 <71875287+sunpungi0623@users.noreply.github.com>
2023-02-15 16:36:06 +0900

    push - update&delete

--- src/main/java/board/board/mapper/BoardMapper.java
+++ src/main/java/board/board/mapper/BoardMapper.java
@@ -11,4 +11,6 @@ public interface BoardMapper {
     void insertBoard(BoardDto board) throws Exception;
     void updateHitCount(int boardIdx) throws Exception;
     BoardDto selectBoardDetail(int boardIdx) throws Exception;
+    void updateBoard(BoardDto board) throws Exception;
+    void deleteBoard(int boardIdx) throws Exception;
 }
